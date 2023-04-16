3a5c66467dd706107797d453ad2611e5e606f560
krasmich <kraskovmichael@gmail.com>
2023-04-16 19:39:41 +0400

    add get and post requests

--- framework/main.py
+++ framework/main.py
@@ -1,3 +1,8 @@
+from quopri import decodestring
+
+from framework.requests.get_request import GetRequests
+from framework.requests.post_request import PostRequests
+
 
 class PageNotFound404:
     def __call__(self, request):
@@ -18,13 +23,36 @@ class App:
         if not path.endswith('/'):
             path = f'{path}/'
 
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = App.decode_value(data)
+            print(f'Пришёл post-запрос: {App.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = App.decode_value(request_params)
+            print(f'Пришли GET-параметры:'
+                  f' {App.decode_value(request_params)}')
+
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
-        request = {}
+
         for front in self.fronts_lst:
             front(request)
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
