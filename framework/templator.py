3b51697f552890a923e3e1ddaf585a5842a31d39
krasmich <kraskovmichael@gmail.com>
2023-04-19 19:33:43 +0400

    work with tamplates

--- framework/templator.py
+++ framework/templator.py
@@ -1,16 +1,14 @@
-from jinja2 import Template
-from os.path import join
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
     """
     :param template_name: имя шаблона
-    :param folder: папка, в которой ищем шаблон
-    :param kwargs: параметры
+    :param kwargs: параметры для передачи в шаблон
     :return:
     """
-    file_path = join(folder, template_name)
-
-    with open(file_path, encoding='utf-8') as f:
-        template = Template(f.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
